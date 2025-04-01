0c2c9f38ee91b73d6ffcdda7352370bc39bc20a9
Simon Ambrozak <simon.ambrozak@gmail.com>
2025-03-31 23:10:19 -0400

    Added git ignore and started steam data script

---
+++ readsteamdata.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+import re
+from tqdm import tqdm
+
+
+# Merge the simple CSVs based on app_id
+gamesdf = pd.read_csv("./games.csv", quotechar='"', escapechar='\\', dtype="str")
+genresdf = pd.read_csv("./genres.csv", quotechar='"', escapechar='\\', dtype="str")
+gamesdf = pd.merge(gamesdf, genresdf, on="app_id")
+reviewsdf = pd.read_csv("./genres.csv", quotechar='"', escapechar='\\', dtype="str")
+gamesdf = pd.merge(gamesdf, reviewsdf, on="app_id")
+
+### TODO: NA values are written as \N. This screws up functions like the one below:
+#gamesdf["release_date"] = pd.to_datetime(gamesdf["release_date"])
+
+### TODO: Fix typing of each column
+
+### Tags and categories need some pre-processing before being added to the DF
+### Not sure if we want to use steamspy, promotional, or descriptions CSVs yet
